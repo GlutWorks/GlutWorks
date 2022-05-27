efb159b7f1b7733a48ccf45ab4d25a111bc2e379
Ierion <rb221694@email.vccs.edu>
2022-05-27 13:27:42 -0400

    Junk & Resource Item Framework
    Includes stackability

---
+++ ixhl2rp/plugins/ixcraft/items/iron_junk/sh_iron3_junk.lua
@@ -0,0 +1,5 @@
+ITEM.name = "Large Iron Sheet"
+ITEM.description = "A large sheet of iron from an unknown source."
+ITEM.model = Model("models/props_combine/combine_explosiveplanels_001a")
+ITEM.width = 2
+ITEM.height = 1
---
+++ ixhl2rp/plugins/ixcraft/items/wooden_junk/sh_wood1_junk.lua
@@ -0,0 +1,5 @@
+ITEM.name = "Large Wooden Piece"
+ITEM.description = "You don't know how part of a boat got into the city, but the wood can be useful."
+ITEM.model = Model("models/props_canal/boat001b_01")
+ITEM.width = 2
+ITEM.height = 1
