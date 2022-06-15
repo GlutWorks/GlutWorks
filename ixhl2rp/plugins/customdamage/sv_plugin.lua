e4480381a54941522be763430e0bceb6be925df1
Oebelysk <bandy.bryce@gmail.com>
2022-06-15 16:47:10 -0400

    Update sv_plugin.lua

---
+++ ixhl2rp/plugins/customdamage/sv_plugin.lua
@@ -0,0 +1,61 @@
+
+local PLUGIN = PLUGIN
+
+function PLUGIN:EntityTakeDamage(target, dmg)
+
+	-- show blood effects if player is ragdoll and decrease overall damage to ragdolls
+	if(target.ixRagdoll) then
+		dmg:ScaleDamage(0.5)
+		local effectBloodSpray = EffectData()
+			effectBloodSpray:SetStart(dmg:GetDamagePosition())
+			effectBloodSpray:SetOrigin(dmg:GetDamagePosition())
+			effectBloodSpray:SetFlags(3)
+			effectBloodSpray:SetColor(0)
+			effectBloodSpray:SetScale(6)
+   		util.Effect("bloodspray", effectBloodSpray)
+		local effectBloodSplatter = EffectData()
+			effectBloodSplatter:SetStart(dmg:GetDamagePosition())
+			effectBloodSplatter:SetOrigin(dmg:GetDamagePosition())
+			effectBloodSplatter:SetScale(10)
+   		util.Effect("BloodImpact", effectBloodSplatter)
+	end
+
+	-- fall over on fall damage
+	if (target:IsPlayer() and ix.config.Get("falloverAfterFallDamage") and target:Health() > dmg:GetDamage() and dmg:IsFallDamage()) then
+		armor = target:Armor()
+		target:SetArmor( 0 )
+		target:SetRagdolled(true, ix.config.Get("fallDamageBaseTime") + dmg:GetDamage() * ix.config.Get("fallDamageTimeMultiplier"))
+		target:TakeDamage( dmg:GetDamage(), worldspawn, worldspawn )
+		target:GodEnable()
+		timer.Simple( 1, function() target:GodDisable() end )
+		target:SetArmor( armor )
+	end
+
+	-- prevent players from taking unneccesary damage after their ragdoll falls over
+	if(target.ixRagdoll == nil and target:IsPlayer() and (target:Health() - dmg:GetDamage()) > 0 and (target:Health() - dmg:GetDamage()) < 20 ) then
+		timer.Simple( 0.1, function()
+			target:GodEnable()
+		end )
+		timer.Simple( 0.3, function()
+			target:GodDisable()
+		end )
+		target:SetRagdolled(true, 100)
+	end
+
+	-- make headshot noise
+	if (target:IsPlayer() and target:LastHitGroup() == 1) then
+		local pitch = math.random(75, 125)
+		target:EmitSound("physics/body/body_medium_break3.wav", 75, pitch, 1)
+	end
+end
+
+-- crouch noise decrease
+function PLUGIN:EntityEmitSound(target)
+    if target.Entity:IsPlayer() and target.Entity:Crouching() and string.find(target.SoundName, "player/footsteps") then
+        target.Volume = target.Volume * .25
+        return true
+    end
+end
+
+function PLUGIN:CreateRagdoll()
+end
