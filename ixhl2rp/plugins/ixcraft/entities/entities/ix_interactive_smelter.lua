a1a76d8c05f90e25592bf858f3902e4ba8c339c7
Ierion0 <KarekOrneal@gmail.com>
2023-03-03 23:50:20 -0500

    Interactive station example

--- ixhl2rp/plugins/ixcraft/entities/entities/ix_interactive_smelter.lua
+++ ixhl2rp/plugins/ixcraft/entities/entities/ix_interactive_smelter.lua
@@ -6,13 +6,124 @@ ENT.Category = "HL2 RP"
 ENT.Spawnable = true
 ENT.AdminOnly = true
 
-function ENT:SetupDataTables()
-	self:NetworkVar("Int", 0, "Display")
-	self:NetworkVar("Bool", 1, "Enabled")
+resouces = {
+	["metal_scrap"] = 0,
+	["iron1_junk"] = 0,
+	["iron2_junk"] = 0,
+	["iron3_junk"] = 0,
+	["iron4_junk"] = 0,
+	["iron5_junk"] = 0,
+	["iron6_junk"] = 0,
+	["iron7_junk"] = 0
+}
+
+smeltWin = {}
+
+if (SERVER) then
+	util.AddNetworkString( "glutAddResource" )
+	net.Receive( "glutAddResource", function(_, player)
+		addResouce(player, net.ReadString():sub(2,-1), tonumber(net.ReadInt(8)))
+	end )
+
+	function addResouce(player, name, amt)
+		print("a"..amt)
+
+		if amt < 0 then amt = 0 end -- intentional redundency
+			
+		local totalAmt = 0
+		for _, item in pairs(player:GetCharacter():GetInventory():GetItems()) do
+			print(item.uniqueID.." vs "..name.."|")
+			if item.uniqueID == name then
+				print("found "..name)
+				if (item:GetData("quantity", 1)) then
+					totalAmt = totalAmt + item:GetData("quantity", 1)
+				else							-- this is 
+					print("f")					-- useless atm			
+					totalAmt = totalAmt + 1		-- but preserved for 
+				end								-- safety		
+				print(totalAmt)
+			end
+		end
+		
+		if totalAmt < amt then
+			player:Notify("You don't have enough "..name.." to smelt!")
+			return
+		end
+
+		resouces[name] = resouces[name] + amt
+		for _, item in pairs(player:GetCharacter():GetInventory():GetItems()) do
+			print(item.uniqueID)
+			if item.uniqueID == name then
+				if item:GetData("quantity", 1) > amt then
+					item:SetData("quantity", item:GetData("quantity", 1) - amt)
+					break
+				else
+					amt = amt - item:GetData("quantity", 1)
+					item:Remove()
+				end
+			end
+		end
+
+	end
+
 end
 
-function smelt()
-    
+
+function smeltWin.OpenWindow()
+	if IsValid(smeltWin.Menu) then
+		smeltWin.Menu:Remove()
+	end
+	local scrw, scrh = ScrW(), ScrH()
+	smeltWin.Menu = vgui.Create("DFrame")
+	smeltWin.Menu:SetSize(scrw * 0.5, scrh * 0.5)
+	smeltWin.Menu:Center()
+	smeltWin.Menu:SetTitle("Smelter")
+	smeltWin.Menu:SetDraggable(true)
+	smeltWin.Menu:MakePopup(true)
+	smeltWin.Menu:ShowCloseButton(true)
+
+	enterResourceAmt = {}
+	for name, _ in pairs(resouces) do
+		enterResourceAmt[name] = smeltWin.Menu:Add("DTextEntry", frame)
+	end
+
+	local resourceList = smeltWin.Menu:Add("DListView")
+	resourceList:Dock(FILL)
+	resourceList:SetMultiSelect(false)
+	resourceList:AddColumn("Resource")
+	resourceList:AddColumn("Amount")
+	resourceList:AddColumn("Add Amount")
+	for name, amount in pairs(resouces) do
+		resourceList:AddLine(name, amount, enterResourceAmt[name])
+	end
+
+	local startBut = smeltWin.Menu:Add("DButton")
+	startBut:SetText("Start Smelting")
+	startBut:Dock(BOTTOM)
+	startBut.DoClick = function()
+		for name, _ in pairs(resouces) do
+			local amt = tonumber(enterResourceAmt[name]:GetText())
+			if amt == nil then
+				continue
+			elseif amt <= 0 then 
+				continue
+			end
+			print(name..": "..amt)
+			net.Start("glutAddResource")
+				net.WriteUInt(LocalPlayer():UserID(), 8)
+				net.WriteString(name)
+				net.WriteInt(amt, 8)
+			net.SendToServer()
+		end
+	end
+end
+
+function ENT:Use(client)
+	print ("Use")
+	if (client:GetPos():Distance(self:GetPos()) <= 128) then
+		print (client:UserID())
+		client:SendLua("smeltWin.OpenWindow()")
+	end
 end
 
 
