d67e75680f511a6c322ccef3b37af26c40a53f6b
Bryce <bandy.bryce@gmail.com>
2023-03-05 16:43:40 -0500

    Citizens are messy eaters, poison makes you vomit

--- ixhl2rp/plugins/survivaladditions/items/eatable/sh_poison_headcrab_gland.lua
+++ ixhl2rp/plugins/survivaladditions/items/eatable/sh_poison_headcrab_gland.lua
@@ -14,10 +14,30 @@ ITEM.functions.Eat = {
 		character:SetHunger(math.Clamp(character:GetHunger() + item.restoredHunger, 0, 100))
 		client:EmitSound("npc/antlion_grub/squashed.wav", 75, 150, 0.25)
 
+        local effect = EffectData()
+            effect:SetStart(client:EyePos())
+            effect:SetOrigin(client:EyePos())
+            effect:SetNormal(client:EyeAngles():Forward())
+            effect:SetScale(0.25)
+        util.Effect("StriderBlood", effect)
+
         timer.Create("PoisonTicker"..item:GetID(), 1.5, 8, function() 
+            print(IsValid(client.ixRagdoll))
+
             if (!client:Alive()) then
                 timer.Remove("PoisonTicker"..item:GetID())
             end
+
+            // don't show the particle effects if the client is ragdolled
+            // at least until I can find a way to attach the particle effect to their face
+            if (!IsValid(client.ixRagdoll)) then
+                local effectPoison = EffectData()
+                    effectPoison:SetStart(client:EyePos())
+                    effectPoison:SetOrigin(client:EyePos())
+                    effectPoison:SetNormal(client:EyeAngles():Forward())
+                    effectPoison:SetScale(0.25)
+                util.Effect("StriderBlood", effectPoison)
+            end  
             client:TakeDamage(8)
         end)
 		return true
