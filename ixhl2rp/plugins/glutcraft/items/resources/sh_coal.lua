d7e32bba51e381638ac3448ccf53d80448f84306
Ierion0 <KarekOrneal@gmail.com>
2023-03-05 00:11:21 -0500

    Migrating crafting system to glutCraft

---
+++ ixhl2rp/plugins/glutcraft/items/resources/sh_coal.lua
@@ -0,0 +1,3 @@
+ITEM.name = "Coal"
+ITEM.description = "A reliable source of energy, but not very useful for anything else."
+ITEM.model = Model("models/mosi/fallout4/props/junk/components/adhesive.mdl")
